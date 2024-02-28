f9d7761a823e6a02f20622e023ae2dbb2ff42f25
louy999 <louyhany999@gmail.com>
2024-02-29 01:17:58 +0200

    finshed

--- client/app/utils/api.ts
+++ client/app/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://18.205.244.53:5000/api",
 });
 
 export default axiosClient;
